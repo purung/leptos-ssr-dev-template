a3307b30c45884748d09d04967418daea9549561
Oliver Nordh <oliver.nordh@proton.me>
2023-11-15 12:10:58 +0100

    Font work - maybe not the best solution

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     files: ["*.html", "./src/**/*.rs"],
   },
   theme: {
-    extend: {},
+    extend: {
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        hoc: {
+         neutral: '#090606',
+         'base-100': '#f7f3f3',
+         primary: '#7ea6a9',
+         secondary: '#dac9c8',
+         accent: '#567e81',
+        },
+      },
+      "dark"
+    ],
   },
   plugins: [require("daisyui")],
 }
