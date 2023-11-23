a18ca147f57f78ddc23346e82db1b27223af7d95
Oliver Nordh <oliver.nordh@proton.me>
2023-11-23 20:11:39 +0100

    exempel seed

---
+++ examples/seed.rs
@@ -0,0 +1,34 @@
+
+use anyhow::Result;
+use chrono::Local;
+use sqlx::postgres::PgPoolOptions;
+use ulid::Ulid;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let name: &'static str = "Inigo Montoya";
+    let tel: &'static str = "0707123728";
+    let special: &'static str = "Prepare to die";
+
+    let pool = PgPoolOptions::new().connect(env!("DATABASE_URL")).await?;
+
+    for _ in 0..5 {
+        let ulid = Ulid::new().to_string();
+        let now = Local::now();
+        sqlx::query(
+            r#"
+            insert into contact_request (uuid, name, tel, special, timestamp)
+            values ($1, $2, $3, $4, $5)
+            "#,
+        )
+        .bind(ulid)
+        .bind(name)
+        .bind(tel)
+        .bind(special)
+        .bind(now)
+        .execute(&pool)
+        .await?;
+    }
+
+    Ok(())
+}
